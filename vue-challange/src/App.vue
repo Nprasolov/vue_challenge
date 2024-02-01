7a555d201ed3708770b1d54e68d66ab7c12531df
nikita.prasolov <nikita.prasolov@thetoadman.com>
2024-02-01 18:17:20 +0300

    Suggestion for https://vuejs-challenges.netlify.app/questions/10-lifecycle/README.html

--- vue-challange/src/App.vue
+++ vue-challange/src/App.vue
@@ -1,30 +1,57 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!--<script setup>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+<!--import Child from "./components/lifecyclehooks/Child.vue";-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <a href="https://vitejs.dev" target="_blank">-->
+<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+<!--    </a>-->
+<!--    <a href="https://vuejs.org/" target="_blank">-->
+<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+<!--    </a>-->
+<!--  </div>-->
+<!--  <HelloWorld msg="Vite + Vue" />-->
+<!--  <Child></Child>-->
+<!--</template>-->
+
+<!--<style scoped>-->
+<!--.logo {-->
+<!--  height: 6em;-->
+<!--  padding: 1.5em;-->
+<!--  will-change: filter;-->
+<!--  transition: filter 300ms;-->
+<!--}-->
+<!--.logo:hover {-->
+<!--  filter: drop-shadow(0 0 2em #646cffaa);-->
+<!--}-->
+<!--.logo.vue:hover {-->
+<!--  filter: drop-shadow(0 0 2em #42b883aa);-->
+<!--}-->
+<!--</style>-->
+<script setup lang="ts">
+import { ref, provide } from "vue"
+import Child from "./components/lifecyclehooks/Child.vue";
+
+const visible = ref(true)
+const timer = ref(null)
+const count = ref(0)
+provide("timer", timer)
+provide("count", count)
+
+function toggle() {
+  visible.value = !visible.value
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Child v-if="visible" />
+    <p>
+      <button @click="toggle">
+        Toggle Child Component
+      </button>
+    </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ vue-challange/src/components/lifecyclehooks/Child.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {onMounted, inject, onUnmounted} from "vue"
+
+  const timer = inject('timer')
+  const count = inject('count')
+
+  onMounted(() => {
+    console.log(`component mounted.`)
+    timer.value = window.setInterval(() => {
+      count.value++
+    }, 1000)
+  })
+
+  onUnmounted(() => {
+    console.log(`component unmounted`);
+    window.clearInterval(timer.value)
+  })
+</script>
+
+<template>
+  <div>
+    <p>
+      Child Component: {{ count }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
